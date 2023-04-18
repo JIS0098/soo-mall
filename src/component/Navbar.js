f4466887c25cc8b54fd8187d571c40a99d3680c5
JIS0098 <114739219+JIS0098@users.noreply.github.com>
2023-04-18 17:08:18 +0900

    test

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
+import {faSearch} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+  const menuList = [
+    "여성",
+    "Divided",
+    "남성",
+    "신생아/유아",
+    "아동",
+    "H&M HOME",
+    "Sale",
+    "지속가능성",
+  ];
   return (
     <div>
       <header>
@@ -11,10 +22,21 @@ const Navbar = () => {
           <div>로그인</div>
         </div>
         <div className="nav-logo">
-            <img width={100} src="https://logos-world.net/wp-content/uploads/2020/04/HM-Logo-1999-present.jpg"/>
+          <img
+            width={100}
+            src="https://logos-world.net/wp-content/uploads/2020/04/HM-Logo-1999-present.jpg"
+          />
         </div>
-        <div>
-            
+        <div className="nav-menu-area">
+          <ul className="nav-menu-list">
+            {menuList.map((item) => (
+              <li>{item}</li>
+            ))}
+          </ul>
+          <div className="nav-menu-search">
+            <FontAwesomeIcon icon={faSearch} />
+            <input type="text" placeholder="제품검색" />
+          </div>
         </div>
       </header>
     </div>
